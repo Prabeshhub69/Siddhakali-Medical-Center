0dcffb1985002dcfff803636c9eeab7e58ca3b21
Prabeshhub69 <prabeshbarca123@gmail.com>
2026-01-04 22:05:33 +0545

    parallax effect 90 percent done

--- js/home.js
+++ js/home.js
@@ -1,15 +1,9 @@
-let ticking = false;
-const speed = 0.25;
+const parallax = document.querySelector('.parallax');
+const media = document.querySelector('.parallax-media');
+const speed = 0.3;
 
-window.addEventListener("scroll", () => {
-  if (!ticking) {
-    requestAnimationFrame(() => {
-      const scrolled = window.pageYOffset;
-      document.querySelectorAll(".parallax").forEach(el => {
-        el.style.backgroundPositionY = `${-(scrolled * speed)}px`;
-      });
-      ticking = false;
-    });
-    ticking = true;
-  }
+window.addEventListener('scroll', () => {
+  const rect = parallax.getBoundingClientRect();
+  const offset = rect.top * speed;
+  media.style.transform = `translateY(${offset}px)`;
 });
