0a7f0714b4f545fc743fb489765611ae1826f480
Prabeshhub69 <prabeshbarca123@gmail.com>
2026-01-20 17:06:37 +0545

    our services component done

--- js/home.js
+++
@@ -1,9 +0,0 @@
-const parallax = document.querySelector('.parallax');
-const media = document.querySelector('.parallax-media');
-const speed = 0.3;
-
-window.addEventListener('scroll', () => {
-  const rect = parallax.getBoundingClientRect();
-  const offset = rect.top * speed;
-  media.style.transform = `translateY(${offset}px)`;
-});
